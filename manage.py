420aa1ce6b636147f43fd973256eb959f526f733
Eugene <yeo.eugene.oey@gmail.com>
2013-02-19 01:18:59 +0800

    Include Lib to Path

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 import os, sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gohome.settings')
+sys.path.insert(0,'lib')
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
