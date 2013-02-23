7f19693d8d96b4a9b5e1370b82d33d7d6c143cc0
Eugene <yeo.eugene.oey@gmail.com>
2013-02-24 01:25:25 +0800

    Add buses to the views

--- gohome/views.py
+++ gohome/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import TemplateView
+from transit.models import Bus
 
 
 class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
-        return super(HomeView, self).get_context_data(**kwargs)
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['buses'] = Bus.all()
+        return context
     template_name = 'home.html'
