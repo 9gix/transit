a48934c064230cbfd7c89c90f3db03092ea057a6
Eugene <yeo.eugene.oey@gmail.com>
2013-03-03 01:22:36 +0800

    Add direction to django admin:

---
+++ directions/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from directions.models import Bus, Stop
+
+admin.site.register(Bus)
+admin.site.register(Stop)
--- directions/models.py
+++ directions/models.py
@@ -6,6 +6,9 @@ class Bus(models.Model):
     operator = models.CharField(max_length=10)
     stops = models.ManyToManyField('Stop')
 
+    class Meta:
+        verbose_name_plural = 'buses'
+
 class Stop(models.Model):
     code = models.CharField(max_length=10)
     lat = models.FloatField()
