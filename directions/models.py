1daaa426f710a064e25efdef7b8dfe084b451354
Eugene <yeo.eugene.oey@gmail.com>
2013-03-03 03:14:19 +0800

    Remove Operator fields

--- directions/models.py
+++ directions/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Bus(models.Model):
     no = models.CharField(max_length=5)
     direction = models.IntegerField()
-    operator = models.CharField(max_length=10)
     stops = models.ManyToManyField('Stop')
 
     class Meta:
