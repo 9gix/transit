939ae32a9905dfbd8f7f7283f61ac34dc31212ce
Eugene <yeo.eugene.oey@gmail.com>
2013-03-03 03:57:20 +0800

    Add Maps Template

---
+++ transit/static/js/directions.js
@@ -0,0 +1,36 @@
+function initialize(){
+    var opt = {
+        center: new google.maps.LatLng(1.35188,103.820114),
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map'), opt);
+
+    var kmlLayersArray = [];
+
+    function addKmlLayer(serv_no_serv_dir) {
+        kmlLayer = new google.maps.KmlLayer('http://publictransport.sg/kml/busroutes/'+serv_no_serv_dir+'.kml');
+        kmlLayer.setMap(map);
+        kmlLayersArray.push(kmlLayer);
+    }
+
+    function deleteKmlLayer(serv_no_serv_dir){
+        kmlLayer = new google.maps.KmlLayer('http://publictransport.sg/kml/busroutes/'+serv_no_serv_dir+'.kml');
+        kmlLayer.setMap(null);
+    }
+
+    $(".bus_chkbox").each(function(){
+        $(this).click(function(){
+            if (this.checked){
+                addKmlLayer(this.value);
+            }else{
+                deleteKmlLayer(this.value);
+            }
+        });
+    });
+
+}
+
+(function(){
+    initialize();
+})();
