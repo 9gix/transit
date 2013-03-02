6b9937a423ec8301dabab44cf675611d978cf8b3
Eugene <yeo.eugene.oey@gmail.com>
2013-03-03 03:21:45 +0800

    Add object represetation name

--- directions/models.py
+++ directions/models.py
@@ -8,7 +8,13 @@ class Bus(models.Model):
     class Meta:
         verbose_name_plural = 'buses'
 
+    def __unicode__(self):
+        return self.no
+
 class Stop(models.Model):
     code = models.CharField(max_length=10)
     lat = models.FloatField()
     lon = models.FloatField()
+
+    def __unicode__(self):
+        return self.code
