3d9897ddff8e78465b380e5c5d0aa04eccbe802f
Eugene <yeo.eugene.oey@gmail.com>
2013-03-03 04:31:38 +0800

    Change to TemplateView

--- directions/views.py
+++ directions/views.py
@@ -1,8 +1,16 @@
-from django.views.generic import View
+from django.views.generic import TemplateView
 from django.shortcuts import render
 from directions.forms import DirectionForm
 
-class DirectionView(View):
-    def get(self, request, *args, **kwargs):
-        form = DirectionForm()
-        return render(request, 'directions/index.html', locals())
+class DirectionView(TemplateView):
+    template_name = 'directions/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(DirectionView, self).get_context_data(**kwargs)
+        params = self.request.GET
+        if params.get('go') == 'Find Bus':
+            direction_from = params.get('direction_from')
+            direction_to = params.get('direction_to')
+
+        context['buses'] = []
+        context['form'] = DirectionForm()
+        return context
