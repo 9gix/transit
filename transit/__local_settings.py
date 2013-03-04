0623d19c690e4c6d80d0fb3e87a836c47fd9962b
Eugene <yeo.eugene.oey@gmail.com>
2013-03-04 22:27:43 +0800

    GIS Database LocalSettings

--- transit/__local_settings.py
+++ transit/__local_settings.py
@@ -6,7 +6,7 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.contrib.gis.db.backends.', # Add 'postgis', 'mysql', 'spatialite' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
